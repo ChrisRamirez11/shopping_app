ca6fa00b94dcc4cb636d87a812315cb83260b8f3
ChrisRamirez11 <chriscasezcacr@gmail.com>
2024-09-04 11:41:45 -0400

    chaotics learnings

--- lib/provider/product_list_provider.dart
+++ lib/provider/product_list_provider.dart
@@ -1,5 +1,6 @@
 import 'package:app_tienda_comida/utils/preferencias_usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProductsListNotifier extends ChangeNotifier {
   PreferenciasUsuario prefs = PreferenciasUsuario();
@@ -12,6 +13,8 @@ class ProductsListNotifier extends ChangeNotifier {
   }
 
   Future<void> _loadList() async {
+    prefs.prefsProductsTypesList =
+        await FetchingProductsTypes().productsTypesList();
     final List<String> list = prefs.prefsProductsTypesList;
     _productsListNotifier = list;
     notifyListeners();
@@ -27,5 +30,25 @@ class ProductsListNotifier extends ChangeNotifier {
   Future<void> _saveList() async {
     prefs.prefsProductsTypesList = productsListNotifier;
     notifyListeners();
+  }
+}
+
+//awesome
+//
+class FetchingProductsTypes {
+  final supabase = Supabase.instance.client;
+  List<String> types = [];
+
+  Future<List<String>> productsTypesList() async {
+    Future<List<Map<String, dynamic>>> response =
+        supabase.from('products').select('type');
+
+    final list = await response.then((value) => value.toList());
+
+    for (var row in list) {
+      types.add(row['type']);
+    }
+
+    return types.toSet().toList();
   }
 }
