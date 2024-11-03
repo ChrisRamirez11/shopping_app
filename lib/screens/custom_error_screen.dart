a6ed7c8d3e751cb8aa06c4cbce5d4dd9f689d877
ChrisCastanedaR <101981955+ChrisRamirez11@users.noreply.github.com>
2024-11-02 20:00:20 -0400

    custom error overflow fixed

--- lib/screens/custom_error_screen.dart
+++ lib/screens/custom_error_screen.dart
@@ -35,57 +35,59 @@ class CustomErrorScreen extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 60,
-          ),
-          Icon(
-            Icons.warning_rounded,
-            size: 200,
-            color: Color(0xFFEDBEAF),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Text(
-            'ERROR',
-            style: TextStyle(fontSize: 30, color: Color(0xFFF9C5B7)),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Text(errorMsg,textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Color(0xFFFCE3D5),)),
-          ),
-          Expanded(child: Container()),
-          ElevatedButton(
-              style: ButtonStyle(
-                  elevation: WidgetStatePropertyAll(10),
-                  padding: WidgetStatePropertyAll(
-                      EdgeInsets.symmetric(horizontal: 50)),
-                  backgroundColor: WidgetStatePropertyAll(Color(0xFFFC8D82))),
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeSreen(),
-                  ),
-                  (route) => false,
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Text('Menù Principal',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Color(0xFFFCE3D5))),
-              )),
-          SizedBox(
-            height: 150,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Icon(
+              Icons.warning_rounded,
+              size: 200,
+              color: Color(0xFFEDBEAF),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'ERROR',
+              style: TextStyle(fontSize: 30, color: Color(0xFFF9C5B7)),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(errorMsg,textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Color(0xFFFCE3D5),)),
+            ),
+            Expanded(child: Container()),
+            ElevatedButton(
+                style: ButtonStyle(
+                    elevation: WidgetStatePropertyAll(10),
+                    padding: WidgetStatePropertyAll(
+                        EdgeInsets.symmetric(horizontal: 50)),
+                    backgroundColor: WidgetStatePropertyAll(Color(0xFFFC8D82))),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomeSreen(),
+                    ),
+                    (route) => false,
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Text('Menù Principal',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Color(0xFFFCE3D5))),
+                )),
+            SizedBox(
+              height: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
